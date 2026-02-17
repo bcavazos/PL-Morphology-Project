0480ea8caf0a71b7c15083aa2e705b5540daa5b0
njlyon0 <njlyon@alumni.iastate.edu>
2026-02-17 18:38:00 -0500

    feat (esa26): draft analyses for core hypotheses

--- scripts/preliminary-analysis_esa-2026.R
+++ scripts/preliminary-analysis_esa-2026.R
@@ -3,7 +3,7 @@
 ## -------------------------------------------------------- ##
 # Purpose:
 ## Preliminary Analysis of PL Data for ESA Abstract
-## Three morphological traits are "leaf_area_cm2", "infl_length_cm", and "int_length"
+## Three morphological traits are "leaf_area_cm2", "infl_length_cm", and "int_length_cm"
 ## We want to know how these change in native vs invasive ranges (range column)
 
 # Hypothesis:
@@ -15,7 +15,10 @@
 
 # Load libraries
 ## install.packages("librarian")
-librarian::shelf(tidyverse)
+librarian::shelf(tidyverse, supportR)
+
+# Create necessary folders
+dir.create(path = file.path("graphs"), showWarnings = F)
 
 # Clear environment + collect garbage
 rm(list = ls()); gc()
@@ -43,14 +46,46 @@ pl_v02 <- pl_v01 %>%
   dplyr::mutate(range = dplyr::case_when(
     range %in% c("invasive",  "invasive ",  "invasive ", "onvasive ") ~ "invasive",
     range %in% c( "native", "native ") ~ "native",
-    nchar(range) == 0 ~ NA,
-    T ~ NA))
+    T ~ range)) %>% 
+  # Keep only known native/invasive range
+  dplyr::filter(range %in% c("native", "invasive"))
 
 # Re-check QC efforts
 sort(unique(pl_v02$range))
 
 # General structure check
 dplyr::glimpse(pl_v02)
+
+## ------------------------------------------ ##
+# Test Core Hypotheses ----
+## ------------------------------------------ ##
+
+# Leaf area test
+lfar_mod <- lm(leaf_area_cm2 ~ range, data = pl_v02)
+stats::anova(lfar_mod)
+
+# Inflorescence length test
+infl_mod <- lm(infl_length_cm ~ range, data = pl_v02)
+stats::anova(infl_mod)
+
+# Internode length test
+node_mod <- lm(int_length_cm ~ range, data = pl_v02)
+stats::anova(node_mod)
+
+## ------------------------------------------ ##
+# Summarize & Graph Results ----
+## ------------------------------------------ ##
+
+# Get summarized versions of the data for each response variable
+(lfar_mean <- supportR::summary_table(data = pl_v02, groups = "range",
+  response = "leaf_area_cm2", drop_na = T))
+(infl_mean <- supportR::summary_table(data = pl_v02, groups = "range",
+  response = "infl_length_cm", drop_na = T))
+(node_mean <- supportR::summary_table(data = pl_v02, groups = "range",
+  response = "int_length_cm", drop_na = T))
+
+# Make some graphs!
+
 
 
 # End ----
