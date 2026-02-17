2216a43ed55127bf05e010dc137ec7d340c19d23
njlyon0 <njlyon@alumni.iastate.edu>
2026-02-17 18:08:23 -0500

    refactor (merge): integrate reproducibility best practices

--- scripts/merge_group_data.R
+++ scripts/merge_group_data.R
@@ -9,23 +9,22 @@ setwd("~/OneDrive - Stonehill College/UndergradResearch/Jules-MorphologyProject"
   # different on your computer
 
 # load libraries
-library(tidyverse)
+## install.packages("librarian")
+librarian::shelf(tidyverse, supportR)
 
 # read in data
-apapane <- read.csv("data/raw data/apapane_data.csv")
-iiwi <- read.csv("data/raw data/iiwi_data.csv")
-nene <- read.csv("data/raw data/nene_data.csv")
-pueo <- read.csv("data/raw data/pueo_data.csv")
+apapane <- read.csv(file.path("data", "raw data", "apapane_data.csv"))
+iiwi <- read.csv(file.path("data", "raw data", "iiwi_data.csv"))
+nene <- read.csv(file.path("data", "raw data", "nene_data.csv"))
+pueo <- read.csv(file.path("data", "raw data", "pueo_data.csv"))
 
 # check that column names are the same
 
 # datasets with infl length
-setdiff(names(apapane), names(pueo))
-setdiff(names(pueo), names(apapane))
+supportR::diff_check(names(apapane), names(pueo))
 
 # datasets w/ internode
-setdiff(names(iiwi), names(nene))
-setdiff(names(nene), names(iiwi))
+supportR::diff_check(names(iiwi), names(nene))
 
 # make object that lists all the new_name = oldname
 names <- c(range = "Range", 
@@ -39,7 +38,7 @@ names <- c(range = "Range",
 # standardize column names across four groups
 pueo <- pueo %>%
   dplyr::rename(any_of(names)) %>%
-  select(-Processed.) # get rid of processed column
+  dplyr::select(-Processed.) # get rid of processed column
 
 apapane <- apapane %>%
   dplyr::rename(any_of(names))
@@ -52,14 +51,16 @@ nene <- nene %>%
 
 # combine all four data sets 
 # should end with a data set that is 19 variables long and 947 rows long
-join1 <- full_join(apapane, pueo)
-join2 <- full_join(join1, nene)
-finaljoin <- full_join(join2, iiwi)
+join1 <- dplyr::full_join(apapane, pueo)
+join2 <- dplyr::full_join(join1, nene)
+finaljoin <- dplyr::full_join(join2, iiwi)
 
 # fix ordering of columns
 finaljoin <- finaljoin %>%
-  relocate(int_length_cm, .before = initials)
+  dplyr::relocate(int_length_cm, .before = initials)
 
 # write csv
-write.csv(finaljoin, "data/raw data/PLdata_full.02.17.26.csv")
+write.csv(x = finaljoin, na = '', row.names = F,
+  file.path("data", "raw data", "PLdata_full.02.17.26.csv"))
  
+# End ----
