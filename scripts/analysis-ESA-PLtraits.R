cb8aa70f81267972b19dfe8d85ae41f34b5a5684
bcavazos <brittany.r.cavazos@gmail.com>
2026-07-23 22:44:28 -0400

    updated analysis and made figures more consistent across traits

--- scripts/analysis-ESA-PLtraits.R
+++ scripts/analysis-ESA-PLtraits.R
@@ -7,7 +7,10 @@
 rm(list = ls()); gc()
 
 # Libraries
-librarian::shelf(tidyverse, lme4, bbmle, lmerTest, emmeans, ggeffects)
+librarian::shelf(tidyverse, lme4, bbmle, lmerTest, emmeans, ggeffects, car)
+
+# color theme: non-native, native
+colors <- c("#762a83", "#7fbf7b")
 
 # Read in tidy data
 traits_v01 <- read.csv("data/PL_traits-tidy.csv") 
@@ -20,30 +23,15 @@ traits_v02 <- traits_v01 %>%
 
 glimpse(traits_v02)
 
-
-# create some data subsets to play around with later -- 
-
-# just samples in the US (to look closer at change over time or if longitude (moving westward) makes a difference)
-PLUS <- traits_v02 %>%
-  filter(range == "invasive")
-
-post_introduction <- traits_v02 %>%
-  filter(year > 1900)
-
 # Date Exploration ----#
 summary(as.factor(traits_v02$country))
-
 summary(as.factor(traits_v02$range))
-
 hist(traits_v02$latitude)
 
 # is latitude correlated with invasive status?
-
 ggplot(traits_v02, aes(x = latitude, fill = range)) +
   geom_histogram(position = "identity", alpha =0.8) +
   theme_classic()
-
-# pretty much entirely...
 
 hist(traits_v02$year)
 
@@ -51,8 +39,6 @@ hist(traits_v02$year)
 ggplot(traits_v02, aes(x = year, fill = range)) +
   geom_histogram(position = "identity", alpha =0.8) +
   theme_classic()
-
-# mostly but not entirely
 
 # representation across month?
 hist(traits_v02$month)
@@ -69,127 +55,245 @@ hist(traits_v02$infl_length_cm) #ish
 
 # Analysis
 
-### Leaf Area ###
+### Leaf Area Analysis ####
   # since range and lat are correlated so tightly, won't put them in the same model
   # include Julian Day 
 
-LA_model1 <- lm(leaf_area_cm2 ~ range * year * julianday, data = traits_v02) 
-LA_model2 <- lm(leaf_area_cm2 ~ range * year + julianday, data = traits_v02) 
-LA_model3 <- lm(leaf_area_cm2 ~ range + year * julianday, data = traits_v02)
-  # interactions are NS
+# LA_model1 <- lm(leaf_area_cm2 ~ year * julianday * range, data = traits_v02) 
+# LA_model2 <- lm(leaf_area_cm2 ~ julianday + range * year, data = traits_v02) 
+# LA_model3 <- lm(leaf_area_cm2 ~ year * julianday + range, data = traits_v02)
 
-LA_model4 <- lm(leaf_area_cm2 ~ range + year + julianday, data = traits_v02) 
-LA_model5 <- lm(leaf_area_cm2 ~ range + year, data = traits_v02) 
-# year is NS
+# LA_model4 <- lm(leaf_area_cm2 ~ year * julianday, data = traits_v02) 
+# LA_model5 <- lm(leaf_area_cm2 ~ julianday * range, data = traits_v02) 
+# LA_model6 <- lm(leaf_area_cm2 ~ year * range, data = traits_v02) 
 
-LA_model6 <- lm(leaf_area_cm2 ~ range + julianday, data = traits_v02) #*
-LA_model7 <- lm(leaf_area_cm2 ~ julianday + year, data = traits_v02) #*
+LA_model7 <- lm(leaf_area_cm2 ~ year + julianday + range, data = traits_v02) 
+# LA_model8 <- lm(leaf_area_cm2 ~ year + range, data = traits_v02) 
 
+LA_model9 <- lm(leaf_area_cm2 ~ julianday + range, data = traits_v02) 
+LA_model10 <- lm(leaf_area_cm2 ~ julianday + year, data = traits_v02)
 
-AICctab(LA_model4, LA_model5, LA_model6, LA_model7, weights = TRUE)
+AICctab(LA_model7, 
+        LA_model9, 
+        LA_model10, weights = TRUE)
 
-summary(LA_model7) # use this for LA results
+# use this for poster -- range is marginally significant and julian day is significant
+car::Anova(LA_model9, type = 2)
+summary(LA_model9) 
 
-summary(LA_model4)
-stats::anova(LA_model6)
+median_iqr <- function(x) {
+  data.frame(
+    y = median(x),
+    ymin = quantile(x, 0.25),
+    ymax = quantile(x, 0.75)
+  )
+}
 
+# Leaf Area between ranges
+LA_range <- ggplot(traits_v02, aes(x = range, y = leaf_area_cm2, fill = range)) +
+  geom_violin(aes(fill = range), 
+              alpha = 0.25, 
+              show.legend = FALSE) +
+  geom_jitter(aes(fill = range, shape = range, color = range), 
+              alpha = 0.45, 
+              width = 0.1, 
+              show.legend = FALSE) +
+  labs(x = "", y = expression("Leaf Area (" * cm^2 * ")")) + 
+  scale_fill_manual(values = colors) + 
+  scale_color_manual(values = colors) +
+  scale_shape_manual(name = "Range", labels = c("Non-native", "Native"), values = c("native" = 21, "invasive" = 23)) + 
+  scale_x_discrete(labels = c("invasive" = "Non-native Range", 
+                              "native" = "Native Range"))+
+  stat_summary(fun.data = median_iqr, geom = "linerange", color = "black", show.legend = FALSE) + 
+  stat_summary(fun = median, geom = "point", color = "black", show.legend = FALSE) + 
+  theme_classic(base_size = 30)
 
+ggsave(
+  "leafarea_ranges.png",
+  plot = LA_range,
+  width = 9,
+  height = 7,
+  units = "in",
+  dpi = 300
+)
 
+# Leaf Area change over time
+LA_season <- ggplot(traits_v02, aes(x = julianday, y = leaf_area_cm2)) +
+  geom_point(aes(fill = range, color = range, shape = range), alpha = 0.45) +
+  labs(x = "Month", y = expression("Leaf Area (" * cm^2 * ")")) + 
+  scale_color_manual(name = "Range", labels = c("Non-native", "Native"), values = colors) +
+  scale_fill_manual(name = "Range", labels = c("Non-native", "Native"), values = colors) +
+  scale_shape_manual(name = "Range", labels = c("Non-native", "Native"), values = c("native" = 21, "invasive" = 23)) +
+  geom_smooth(method = "lm", se = FALSE, color = "black") + 
+  scale_x_continuous(
+    breaks = c(121, 152, 182, 213, 244, 274, 305),
+    labels = c("May", "Jun",
+               "Jul", "Aug", "Sep", "Oct", "Nov"
+    )) +
+  theme_classic(base_size = 30) +
+  theme(legend.position = "inside", legend.position.inside = c(0.2,0.9)) 
+  
 
-# check model fit***
+ggsave(
+  "leafarea_season.png",
+  plot = LA_season,
+  width = 9,
+  height = 7,
+  units = "in",
+  dpi = 300
+)
 
-# Internode second
+# Internode Analysis ####
 
+# INT_model1  <- lm(int_length_cm ~ range * year * julianday, data = traits_v02) 
+INT_model2 <- lm(int_length_cm ~ julianday + range * year, data = traits_v02) # NS
+INT_model3 <- lm(int_length_cm ~ year * julianday + range, data = traits_v02) # NS
 
-INT_model1  <- lm(int_length_cm ~ range * year * julianday, data = traits_v02) 
-INT_model2 <- lm(int_length_cm ~ range * year + julianday, data = traits_v02) 
-INT_model3 <- lm(int_length_cm ~ range + year * julianday, data = traits_v02)
-# interactions are NS
+# INT_model4  <- lm(int_length_cm ~ range * year, data = traits_v02) 
+# INT_model5  <- lm(int_length_cm ~ year * julianday, data = traits_v02) 
+# INT_model6  <- lm(int_length_cm ~ range * julianday, data = traits_v02) 
 
-INT_model4 <- lm(int_length_cm ~ range + year + julianday, data = traits_v02) 
-INT_model5 <- lm(int_length_cm ~ range + year, data = traits_v02) 
-INT_model6 <- lm(int_length_cm ~ range + julianday, data = traits_v02) #*
-INT_model7 <- lm(int_length_cm ~ julianday + year, data = traits_v02) #*
+INT_model7 <- lm(int_length_cm ~ year + julianday + range, data = traits_v02) 
+# INT_model8 <- lm(int_length_cm ~ year + range, data = traits_v02) 
+INT_model9 <- lm(int_length_cm ~ julianday + range, data = traits_v02) 
+# INT_model10 <- lm(int_length_cm ~ julianday + year, data = traits_v02) 
 
-# stats::anova(INT_model1)
-# stats::anova(INT_model2)
-# stats::anova(INT_model3) 
-stats::anova(INT_model4) #** best model
-stats::anova(INT_model5)
-stats::anova(INT_model6) # second best
-stats::anova(INT_model7)
+AICctab(INT_model2, 
+        INT_model3, 
+        INT_model7, 
+        INT_model9, 
+        weights = TRUE)
 
-# year has strongest affect
-AICctab(INT_model4, INT_model5, INT_model6, INT_model7, weights = TRUE)
-
-summary(INT_model4)
-# model 4 is best fit - the additive range and year
-# internode is longer in native range and smaller over time latitudes and LA has gets shorter over time
+car::Anova(INT_model7, type = 2)
+# model 7 is best fit - the additive range and year and julian day
 
 traits_v02 %>%
   group_by(range) %>%
   summarise(mean = mean(int_length_cm, na.rm = T), 
             sd = sd(int_length_cm, na.rm = T))
 
-
-## check model fit ****
-
-# Infl third
-
-INF_model1  <- lm(infl_length_cm ~ range * year * julianday, data = traits_v02) 
-INF_model2 <- lm(infl_length_cm ~ range * year + julianday, data = traits_v02) 
-INF_model3 <- lm(infl_length_cm ~ range + year * julianday, data = traits_v02)
-# interactions are NS
-INF_model3.5 <- lm(infl_length_cm ~ range * year, data = traits_v02)
-INF_model4 <- lm(infl_length_cm ~ range + year + julianday, data = traits_v02) 
-INF_model5 <- lm(infl_length_cm ~ range + year, data = traits_v02) 
-INF_model6 <- lm(infl_length_cm ~ range + julianday, data = traits_v02) 
-INF_model7 <- lm(infl_length_cm ~ julianday + year, data = traits_v02) 
-INF_model8 <- lm(infl_length_cm ~ range, data = traits_v02)
-
-# stats::anova(INF_model1)
-# stats::anova(INF_model2)
-# stats::anova(INF_model3)
-# stats::anova(INF_model4)
-stats::anova(INF_model5)
-# stats::anova(INF_model6)
-stats::anova(INF_model7)
-
-summary(INF_model8)
-
-
-AICctab(INF_model2, INF_model4, INF_model5, INF_model6, INF_model7, INF_model8, INF_model3.5, weights = TRUE)
-
-summary(INF_model3.5)
-# models 5, 6, 2
-
-# range matters the most but there is a marginally significant interaction between range and year
-
-# check model fit ***
-
-
-### visualization
-
-# Leaf area - just range ####
-colors <- c("#762a83", "#7fbf7b")
-
-# raw data
-ggplot(traits_v02, aes(x = range, y = leaf_area_cm2, fill = range)) +
-  geom_boxplot(show.legend = FALSE) +
-  geom_violin(alpha = 0.25, show.legend = FALSE) +
-  labs(x = "", y = expression("Leaf Area (" * cm^2 * ")")) + 
+# internode range
+INT_range <- ggplot(traits_v02, aes(x = range, y = int_length_cm, fill = range)) +
+  geom_violin(aes(fill = range), 
+              alpha = 0.25, 
+              show.legend = FALSE) +
+  geom_jitter(aes(fill = range, shape = range, color = range), 
+              alpha = 0.45, 
+              width = 0.1, 
+              show.legend = FALSE) +
+  labs(x = "", y = "Internode Length (cm)") + 
   scale_fill_manual(values = colors) + 
-  scale_x_discrete(labels = c(
-    "invasive" = "Non-native Range",
-    "native" = "Native Range"))+
-  theme_classic(base_size = 20)
+  scale_color_manual(values = colors) +
+  scale_shape_manual(name = "Range", labels = c("Non-native", "Native"), values = c("native" = 21, "invasive" = 23)) + 
+  scale_x_discrete(labels = c("invasive" = "Non-native Range", 
+                              "native" = "Native Range"))+
+  stat_summary(fun.data = median_iqr, geom = "linerange", color = "black", show.legend = FALSE) + 
+  stat_summary(fun = median, geom = "point", color = "black", show.legend = FALSE) + 
+  theme_classic(base_size = 30)
+
+ggsave(
+  "INT_range.png",
+  plot = INT_range,
+  width = 9,
+  height = 7,
+  units = "in",
+  dpi = 300
+)
+
+# Internode change over time
+INT_season <- ggplot(traits_v02, aes(x = julianday, y = int_length_cm)) +
+  geom_point(aes(fill = range, color = range, shape = range), alpha = 0.45) +
+  labs(x = "Month", y = "Internode Length (cm)") + 
+  scale_color_manual(name = "Range", labels = c("Non-native", "Native"), values = colors) +
+  scale_fill_manual(name = "Range", labels = c("Non-native", "Native"), values = colors) +
+  scale_shape_manual(name = "Range", labels = c("Non-native", "Native"), values = c("native" = 21, "invasive" = 23)) +
+  geom_smooth(method = "lm", se = FALSE, color = "black") + 
+  scale_x_continuous(
+    breaks = c(121, 152, 182, 213, 244, 274, 305),
+    labels = c("May", "Jun",
+               "Jul", "Aug", "Sep", "Oct", "Nov"
+    )) +
+  theme_classic(base_size = 30) +
+  theme(legend.position = "inside", legend.position.inside = c(0.18,0.88)) 
+
+ggsave(
+  "INT_season.png",
+  plot = INT_season,
+  width = 9,
+  height = 7,
+  units = "in",
+  dpi = 300
+)
+
+
+
+
+# Infl Analysis ####
+
+#INF_model1  <- lm(infl_length_cm ~ range * year * julianday, data = traits_v02) 
+#INF_model2 <- lm(infl_length_cm ~ julianday + range * year, data = traits_v02) 
+#INF_model3 <- lm(infl_length_cm ~ year * julianday + range, data = traits_v02)
+
+INF_model4 <- lm(infl_length_cm ~ range * year, data = traits_v02)
+#INF_model5 <- lm(infl_length_cm ~ year * julianday, data = traits_v02)
+#INF_model6 <- lm(infl_length_cm ~ range * julianday, data = traits_v02)
+
+#INF_model7 <- lm(infl_length_cm ~ julianday + year + range, data = traits_v02) 
+INF_model8 <- lm(infl_length_cm ~  year + range,  data = traits_v02) 
+INF_model9 <- lm(infl_length_cm ~ julianday + range, data = traits_v02) 
+#INF_model10 <- lm(infl_length_cm ~ julianday + year, data = traits_v02) 
+INF_model11 <- lm(infl_length_cm ~ range, data = traits_v02)
+
+
+
+AICctab(INF_model4, 
+        INF_model8, 
+        INF_model9, 
+        INF_model11,
+        weights = TRUE)
+
+# use model 4 bc marginally sig interaction term
+car::Anova(INF_model4, type = 2)
+
+# range matters the most but there is a marginally significant interaction between range and year 
+
+traits_v02 %>%
+  group_by(range) %>%
+  summarise(mean = mean(infl_length_cm, na.rm = T), 
+            sd = sd(infl_length_cm, na.rm = T))
+
+inf_intx <- traits_v02 %>%
+  dplyr::filter(!is.na(infl_length_cm)) %>%
+  ggplot(data = ., aes(x = year, y = infl_length_cm)) +
+  geom_point(aes(fill = range, shape = range, color = range), alpha = 0.45)+
+  geom_smooth(aes(color = range), formula = "y ~ x",
+              method = "lm", se = FALSE) +
+  labs(x = "Year", y = "Inflorescence Length (cm)") + 
+  scale_color_manual(name = "Range", labels = c("Non-native", "Native"), values = colors) +
+  scale_fill_manual(name = "Range", labels = c("Non-native", "Native"), values = colors) +
+  scale_shape_manual(name = "Range", labels = c("Non-native", "Native"), values = c("native" = 21, "invasive" = 23)) +
+  theme_classic(base_size = 30) +
+  theme(legend.justification = "top") 
+
+ggsave(
+  "inf_intx.png",
+  plot = inf_intx,
+  width = 12,
+  height = 6,
+  units = "in",
+  dpi = 300
+)
+
+### visualization extras ####
+
+# Leaf area - ###
 
 # model results w/ 95% CIs
 traits_v02$range <- as.factor(traits_v02$range)
 LAmodel <- lm(leaf_area_cm2 ~ range + julianday, data = traits_v02)
 range_results <- as.data.frame(emmeans(LAmodel, ~ range))
 
-leafarea <- ggplot(range_results, aes(x = range, y = emmean, fill = range, color = range)) +
+ggplot(range_results, aes(x = range, y = emmean, fill = range, color = range)) +
   geom_point(show.legend = FALSE, size = 5, aes(shape = range, fill = range)) +
   geom_errorbar(show.legend = FALSE, aes(ymin = lower.CL, ymax = upper.CL,
     width = 0.2), lwd = 2) +
@@ -200,18 +304,7 @@ leafarea <- ggplot(range_results, aes(x = range, y = emmean, fill = range, color
   scale_x_discrete(labels = c("invasive" = "Non-native Range", "native" = "Native Range")) +
   theme_classic(base_size = 20) 
 
-
-ggsave(
-  "leafarea_modelresults.png",
-  plot = leafarea,
-  width = 7.5,
-  height = 5,
-  units = "in",
-  dpi = 300
-)
-
 # another way to visualize it 
-
 pred <- ggeffects::ggpredict(LAmodel, terms = c("julianday", "range"))
 pred <- as.data.frame(pred)
 pred$julianday <- pred$x
@@ -239,8 +332,7 @@ ggplot() +
        fill = "range") +
   theme_classic()
 
-
-leafarea_modelresults2 <- ggplot(data = traits_v02, aes(x = julianday, y = leaf_area_cm2, color = range)) +
+ggplot(data = traits_v02, aes(x = julianday, y = leaf_area_cm2, color = range)) +
   geom_point(aes(fill = range, shape = range), alpha = 0.45) +
   labs(x = "Month", y = expression("Leaf Area (" * cm^2 * ")")) + 
   scale_color_manual(name = "Range", labels = c("Non-native", "Native"), values = colors) +
@@ -255,13 +347,6 @@ leafarea_modelresults2 <- ggplot(data = traits_v02, aes(x = julianday, y = leaf_
   theme_classic(base_size = 20) +
   theme(legend.justification = "top") 
 
-ggsave( "leafarea_modelresults2.png",
-        plot = leafarea_modelresults2,
-        width = 12,
-        height = 6,
-        units = "in",
-        dpi = 300
-)
 
 # Internode - range + julian day + year ####
 
@@ -273,8 +358,7 @@ pred$range <- pred$group
 pred$julianday <- pred$x
 
 
-
-internode2 <- ggplot(traits_v02, aes(x = julianday, y = int_length_cm, color = range)) +
+ggplot(traits_v02, aes(x = julianday, y = int_length_cm, color = range)) +
   geom_point(aes(fill = range, shape = range), alpha = 0.45) +
   labs(x = "Month", y = "Internode Length (cm)") + 
   scale_color_manual(name = "Range", labels = c("Non-native", "Native"), values = colors) +
@@ -289,31 +373,10 @@ internode2 <- ggplot(traits_v02, aes(x = julianday, y = int_length_cm, color = r
   theme_classic(base_size = 20) +
   theme(legend.justification = "top") 
 
-ggsave(
-  "internode_modelresults2.png",
-  plot = internode2,
-  width = 12,
-  height = 6,
-  units = "in",
-  dpi = 300
-)
 
-# infl - year by range interaction ####
-# actually just range
 
-# raw data
-traits_v02 %>%
-  dplyr::filter(!is.na(infl_length_cm)) %>%
-  ggplot(data = ., aes(x = year, y = infl_length_cm)) +
-  geom_point(aes(fill = range, shape = range), alpha = 0.45)+
-  geom_smooth(aes(color = range), formula = "y ~ x",
-              method = "lm", se = FALSE) +
-  labs(x = "Year", y = "Inflorescence Length (cm)") + 
-  scale_color_manual(name = "Range", labels = c("Non-native", "Native"), values = colors) +
-  scale_fill_manual(name = "Range", labels = c("Non-native", "Native"), values = colors) +
-  scale_shape_manual(name = "Range", labels = c("Non-native", "Native"), values = c("native" = 21, "invasive" = 23)) +
-  theme_classic(base_size = 20) +
-  theme(legend.justification = "top") 
+# infl
+
 
 # model resutls w/ 95% CIs
 model.inf <- lm(infl_length_cm ~ year * range, data = traits_v02)
@@ -324,13 +387,11 @@ newdat.inf <- expand.grid(
 )
 
 pred.inf <- predict(model.inf, newdata = newdat.inf, interval = "confidence")
-
 newdat.inf <- cbind(newdat.inf, pred.inf)
-
 newdat.inf$pred.inf <- predict(model.inf, newdata = newdat.inf)
 
 
-infl_modelresults <- ggplot(traits_v02, aes(x = year, y = infl_length_cm, color = range)) +
+ggplot(traits_v02, aes(x = year, y = infl_length_cm, color = range)) +
   geom_point(aes(fill = range, shape = range), alpha = 0.45) +
   labs(x = "Year", y = "Inflorescence Length (cm)") + 
   scale_color_manual(name = "Range", labels = c("Non-native", "Native"), values = colors) +
@@ -340,14 +401,7 @@ infl_modelresults <- ggplot(traits_v02, aes(x = year, y = infl_length_cm, color 
   theme_classic(base_size = 20) +
   theme(legend.justification = "top") 
 
-ggsave(
-  "infl_modelresults.png",
-  plot = infl_modelresults,
-  width = 12,
-  height = 6,
-  units = "in",
-  dpi = 300
-)
+
 
 
 # just range
@@ -379,7 +433,7 @@ ggplot(traits_v02, aes(x = range, y = infl_length_cm, color = range)) +
        y = "Inflorescence length cm") +
   theme_classic()
 
-infl2<- ggplot(traits_v02, aes(x = range, y = infl_length_cm, fill = range)) +
+ggplot(traits_v02, aes(x = range, y = infl_length_cm, fill = range)) +
   # geom_boxplot(show.legend = FALSE) +
   geom_violin(draw_quantiles = 0.5, alpha = 0.25, show.legend = FALSE, aes(color = range)) +
   geom_jitter(alpha = 0.75, aes(color = range), width = 0.08, show.legend = FALSE)+
@@ -390,18 +444,3 @@ infl2<- ggplot(traits_v02, aes(x = range, y = infl_length_cm, fill = range)) +
     "invasive" = "Non-native Range",
     "native" = "Native Range"))+
   theme_classic(base_size = 20)
-
-
-ggsave(
-  "infl_modelresults2.png",
-  plot = infl2,
-  width = 10,
-  height = 6,
-  units = "in",
-  dpi = 300
-)
-
-traits_v02 %>%
-  group_by(range) %>%
-  summarise(mean = mean(infl_length_cm, na.rm = T), 
-            sd = sd(infl_length_cm, na.rm = T))
